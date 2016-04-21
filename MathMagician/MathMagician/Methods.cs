31b5eae46b4793de4eaeecc6f80027007ad83981
William Hickok <William Hickok>
2016-04-21 15:29:24 -0500

    finished for the day, working on MathMagician

---
+++ MathMagician/MathMagician/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician
+{
+    public class Methods
+    {
+        public void printIntegers()
+        {
+
+        }
+
+        public void printFibonacci()
+        {
+
+        }
+
+        public void printPrimes()
+        {
+
+        }
+    }
+}
---
+++ MathMagician/MathMagicianTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician;
+
+namespace MathMagicianTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SquareTest()
+        {
+            Methods method = new Methods();
+            Assert.IsInstanceOfType(method, typeof(Methods));
+        }
+
+        [TestMethod]
+        public void SquareAreaTest()
+        {
+            Methods newMethod = new Methods();
+
+        }
+    }
+}
